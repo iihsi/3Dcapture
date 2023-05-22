f7e936b0ff6029e6409e5649cfce70c19fe91aba
iihsi <69301521+iihsi@users.noreply.github.com>
2023-05-23 00:30:53 +0900

    Update and rename utils_me.py to utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import numpy as np
 from scipy import linalg
 from scipy.spatial.transform import Rotation
 
-
-# 回転行列の誤差評価関数
 def eval_R_error(estimated_R: np.ndarray, ideal_R: np.ndarray) -> np.float_:
     error_rot = Rotation.from_matrix(estimated_R @ ideal_R.T)
     return linalg.norm(error_rot.as_rotvec())
@@ -12,28 +10,6 @@ def eval_T_error(estimated_T: np.ndarray, ideal_T: np.ndarray) -> np.float_:
     error_rot = estimated_T - ideal_T
     return linalg.norm(error_rot)
 
-
-# RTの正解ファイルからRT(4×4)を取得
-def get_gtRT(file_path):
-    gtRT = []
-    with open(file_path) as f:
-        temp = []
-        for i, line in enumerate(f.readlines()):
-            if i % 5 == 0:
-                continue
-            else:
-                line = line.split()
-                line = line[:4]
-                line = [float(n) for n in line]
-                temp.append(line)
-                if (i + 1) % 5 == 0:
-                    temp = np.array(temp)
-                    gtRT.append(temp)
-                    temp = []
-    return gtRT
-
-
-# RTの正解ファイルからR(3×3)を取得
 def get_gtR(file_path):
     gtR = []
     with open(file_path) as f:
